ad0ea1fae84a268355719feb7327f63d7d21845b
Bonnie Hagenow <200004908@fvtc.edu>
2025-09-06 11:03:47 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector('h1')
+h1.style.fontSize ='3em'
+h1.style.color = 'green'
+h1.style.textAlign = 'center'
+
+const h2 = document.querySelector('h2')
+h2.style.color = 'white'
+
+const pics = document.querySelectorAll('.pics')
+pics.style.borderRadius = ('50%')
